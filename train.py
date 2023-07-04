0b24500948262d964901abd3008e1351c480e78e
cyash12 <chetnani.yash@gmail.com>
2023-07-04 02:05:02 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from transformers import AutoModelForCausalLM
-from peft import get_peft_config, get_peft_model, PromptTuningInit, PromptTuningConfig, TaskType, PeftType
+from peft import get_peft_config, get_peft_model, PromptTuningInit, PromptTuningConfig, TaskType, PeftType, PeftModel, PeftConfig
 import torch
 from datasets import load_dataset
 import os
@@ -8,33 +8,10 @@ from torch.utils.data import DataLoader
 from transformers import default_data_collator, get_linear_schedule_with_warmup
 from tqdm import tqdm
 from datasets import load_dataset
-
-dataset = load_dataset("wiki_qa")
-
-text_column = "question"
-label_column = "answer"
-max_length = 1024
-lr = 3e-2
-num_epochs = 50
-batch_size = 16
-
-
-device = "cuda"
-model_name_or_path = "facebook/opt-125m"
-tokenizer_name_or_path = "facebook/opt-125m"
-peft_config = PromptTuningConfig(
-    task_type=TaskType.CAUSAL_LM,
-    prompt_tuning_init=PromptTuningInit.TEXT,
-    num_virtual_tokens=8,
-    prompt_tuning_init_text="Answer the question: ",
-    tokenizer_name_or_path=model_name_or_path,
-)
-
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-if tokenizer.pad_token_id is None:
-    tokenizer.pad_token_id = tokenizer.eos_token_id
+import sys
 
 def preprocess_function(examples):
+    model_name_or_path = "facebook/opt-125m"
     batch_size = len(examples[text_column])
     inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]]
     targets = [str(x) for x in examples[label_column]]
@@ -43,11 +20,9 @@ def preprocess_function(examples):
     for i in range(batch_size):
         sample_input_ids = model_inputs["input_ids"][i]
         label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
-        # print(i, sample_input_ids, label_input_ids)
         model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
         labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
         model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
-    # print(model_inputs)
     for i in range(batch_size):
         sample_input_ids = model_inputs["input_ids"][i]
         label_input_ids = labels["input_ids"][i]
@@ -64,128 +39,112 @@ def preprocess_function(examples):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
-processed_datasets = dataset.map(
-    preprocess_function,
-    batched=True,
-    num_proc=1,
-    remove_columns=dataset["train"].column_names,
-    load_from_cache_file=False,
-    desc="Running tokenizer on dataset",
-)
+if __name__ == '__main__':
+    dataset = load_dataset("wiki_qa")
+    text_column = "question"
+    label_column = "answer"
+    tokenizer_name_or_path = "facebook/opt-125m"
+    peft_config = PromptTuningConfig(
+    task_type=TaskType.CAUSAL_LM,
+    prompt_tuning_init=PromptTuningInit.TEXT,
+    num_virtual_tokens=8,
+    prompt_tuning_init_text="Answer the question: ",
+    tokenizer_name_or_path=tokenizer_name_or_path,)
+    max_length = 1024
+    lr = 3e-2
+    num_epochs = 1
+    batch_size = 24
+    device = "cuda"
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path)
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    mode = sys.argv[1]
+    if mode == 'train':
+        model_name_or_path = "facebook/opt-125m"
+        processed_datasets = dataset.map(
+            preprocess_function,
+            batched=True,
+            num_proc=1,
+            remove_columns=dataset["train"].column_names,
+            load_from_cache_file=False,
+            desc="Running tokenizer on dataset",)
+        train_dataset = processed_datasets["train"]
+        eval_dataset = processed_datasets["train"]
 
-train_dataset = processed_datasets["train"]
-eval_dataset = processed_datasets["train"]
-
-train_dataloader = DataLoader(
-    train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True
-)
-eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
-
-
-def test_preprocess_function(examples):
-    batch_size = len(examples[text_column])
-    inputs = [f"{text_column} : {x} Ans : " for x in examples[text_column]]
-    model_inputs = tokenizer(inputs)
-    # print(model_inputs)
-    for i in range(batch_size):
-        sample_input_ids = model_inputs["input_ids"][i]
-        model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
-            max_length - len(sample_input_ids)
-        ) + sample_input_ids
-        model_inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + model_inputs[
-            "attention_mask"
-        ][i]
-        model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length])
-        model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length])
-    return model_inputs
-
-
-test_dataset = dataset["test"].map(
-    test_preprocess_function,
-    batched=True,
-    num_proc=1,
-    remove_columns=dataset["train"].column_names,
-    load_from_cache_file=False,
-    desc="Running tokenizer on dataset",
-)
-
-test_dataloader = DataLoader(test_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
-next(iter(test_dataloader))
-
-
-next(iter(train_dataloader))
-
-
-len(test_dataloader)
-
-
-next(iter(test_dataloader))
-
-
-# creating model
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
-model = get_peft_model(model, peft_config)
-model.print_trainable_parameters()
-
-
-# model
-# optimizer and lr scheduler
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-lr_scheduler = get_linear_schedule_with_warmup(
-    optimizer=optimizer,
-    num_warmup_steps=0,
-    num_training_steps=(len(train_dataloader) * num_epochs),
-)
-
-
-# training and evaluation
-model = model.to(device)
-
-for epoch in range(num_epochs):
-    model.train()
-    total_loss = 0
-    for step, batch in enumerate(tqdm(train_dataloader)):
-        batch = {k: v.to(device) for k, v in batch.items()}
-        #         print(batch)
-        #         print(batch["input_ids"].shape)
-        outputs = model(**batch)
-        loss = outputs.loss
-        total_loss += loss.detach().float()
-        loss.backward()
-        optimizer.step()
-        lr_scheduler.step()
-        optimizer.zero_grad()
-
-    model.eval()
-    eval_loss = 0
-    eval_preds = []
-    for step, batch in enumerate(tqdm(eval_dataloader)):
-        batch = {k: v.to(device) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = model(**batch)
-        loss = outputs.loss
-        eval_loss += loss.detach().float()
-        eval_preds.extend(
-            tokenizer.batch_decode(torch.argmax(outputs.logits, -1).detach().cpu().numpy(), skip_special_tokens=True)
+        train_dataloader = DataLoader(
+            train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
+        eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
+        model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+        model = get_peft_model(model, peft_config)
+        model.print_trainable_parameters()
+        optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+        lr_scheduler = get_linear_schedule_with_warmup(
+            optimizer=optimizer,
+            num_warmup_steps=0,
+            num_training_steps=(len(train_dataloader) * num_epochs),
         )
+        model = model.to(device)
+        for epoch in range(num_epochs):
+            model.train()
+            total_loss = 0
+            for step, batch in enumerate(tqdm(train_dataloader)):
+                batch = {k: v.to(device) for k, v in batch.items()}
+                outputs = model(**batch)
+                loss = outputs.loss
+                total_loss += loss.detach().float()
+                loss.backward()
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
 
-    eval_epoch_loss = eval_loss / len(eval_dataloader)
-    eval_ppl = torch.exp(eval_epoch_loss)
-    train_epoch_loss = total_loss / len(train_dataloader)
-    train_ppl = torch.exp(train_epoch_loss)
-    print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
+            model.eval()
+            eval_loss = 0
+            eval_preds = []
+            for step, batch in enumerate(tqdm(eval_dataloader)):
+                batch = {k: v.to(device) for k, v in batch.items()}
+                with torch.no_grad():
+                    outputs = model(**batch)
+                loss = outputs.loss
+                eval_loss += loss.detach().float()
+                eval_preds.extend(
+                    tokenizer.batch_decode(torch.argmax(outputs.logits, -1).detach().cpu().numpy(), skip_special_tokens=True)
+                )
+
+            eval_epoch_loss = eval_loss / len(eval_dataloader)
+            eval_ppl = torch.exp(eval_epoch_loss)
+            train_epoch_loss = total_loss / len(train_dataloader)
+            train_ppl = torch.exp(train_epoch_loss)
+            print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
 
 
-model.eval()
-i = 33
-inputs = tokenizer(f'{text_column} : {dataset["test"][i]["question"]} Ans : ', return_tensors="pt")
-print(dataset["test"][i]["question"])
-#print(inputs)
+        model.eval()
+        inputs = tokenizer(f'{text_column} : {dataset["test"][i]["question"]} Ans : ', return_tensors="pt")
+        print(dataset["test"][i]["question"])
 
-with torch.no_grad():
-    inputs = {k: v.to(device) for k, v in inputs.items()}
-    outputs = model.generate(
-        input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"], max_new_tokens=20, eos_token_id=3
-    )
-    #print(outputs)
-    print(tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True))
+        with torch.no_grad():
+            inputs = {k: v.to(device) for k, v in inputs.items()}
+            outputs = model.generate(
+                input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"], max_new_tokens=20, eos_token_id=3
+            )
+            print(tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True))
+        peft_model_id = f"{model_name_or_path}_{peft_config.peft_type}_{peft_config.task_type}"
+        print(peft_model_id)
+        model.save_pretrained(peft_model_id)
+    elif mode == 'eval':
+        peft_model_id = sys.argv[2]
+        config = PeftConfig.from_pretrained(peft_model_id)
+        model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
+        model = PeftModel.from_pretrained(model, peft_model_id)
+
+
+        model.to(device)
+        model.eval()
+        i = 8
+        inputs = tokenizer(f'{text_column} : {dataset["test"][i]["question"]} Ans : ', return_tensors="pt")
+
+        with torch.no_grad():
+            inputs = {k: v.to(device) for k, v in inputs.items()}
+            outputs = model.generate(
+                input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"], max_new_tokens=10, eos_token_id=3
+            )
+            print(tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True))
